688cfe1fca7bbb271c2ab4d67c4ce2af6886132e
Lehmann-Katharina <86959588+Lehmann-Katharina@users.noreply.github.com>
2021-08-19 17:01:12 +0200

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("you are creating a Vanilla Weather App for SheCodes Plus");
+let apiKey = "7c5a6c2ab59b7561795113f8a11b09a5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+axios.get(apiUrl).then(displayTemperature);
